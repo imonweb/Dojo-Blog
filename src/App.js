eafcfc91e4f7091a92cedff796f3b3d0a835ef93
imonweb <contact@imonweb.co.uk>
2022-03-20 13:44:06 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
  
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-       <div className="content">
-         <h1><Home /></h1>
-       </div>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+             <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,10 @@
-import {useState, useEffect} from 'react';
+// import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 
 const Home = () => {
   const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
-  // const [blogs, setBlogs] = useState(null);
-  // const [isPending, SetIsPending] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     fetch('http://localhost:8000/blogs')
-  //       .then(res => {
-  //         // console.log(res);
-  //         if(!res.ok){
-  //           throw Error('could not fetch the data for that resource')
-  //         }
-  //         return res.json()
-  //       })
-  //       .then(data => {
-  //         setBlogs(data)
-  //         SetIsPending(false)
-  //         setError(null)
-  //       })
-  //       .catch(err => {
-  //         // console.log(err.message);
-  //         SetIsPending(false)
-  //         setError(err.message)
-  //       })
-  //   }, 1000)
-  // }, []);
-
+   
   return ( 
     <div className="home">
       { error && <div>{error}</div>}
