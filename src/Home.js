4a18cf870318a45d1d178a488c5c64348d050ab5
imonweb <contact@imonweb.co.uk>
2022-03-20 11:15:10 +0000

    custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,42 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    { title: 'Web dev top tips!', body: 'lorem ipsum...', author: 'mario', id: 3}
-  ]);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+  // const [blogs, setBlogs] = useState(null);
+  // const [isPending, SetIsPending] = useState(true);
+  // const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  });
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     fetch('http://localhost:8000/blogs')
+  //       .then(res => {
+  //         // console.log(res);
+  //         if(!res.ok){
+  //           throw Error('could not fetch the data for that resource')
+  //         }
+  //         return res.json()
+  //       })
+  //       .then(data => {
+  //         setBlogs(data)
+  //         SetIsPending(false)
+  //         setError(null)
+  //       })
+  //       .catch(err => {
+  //         // console.log(err.message);
+  //         SetIsPending(false)
+  //         setError(err.message)
+  //       })
+  //   }, 1000)
+  // }, []);
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      { error && <div>{error}</div>}
+      { isPending && <div>Loading...</div>}
+      { blogs && <BlogList blogs={blogs} title="All Blogs!"/> }
+      
     </div>
    );
 }
